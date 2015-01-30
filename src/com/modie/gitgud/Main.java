9f84dde849a110c07c7a8b82d890e8b2ed65c026
modie <moodyr64>
2015-01-30 09:47:34 +0200

    Buttons

--- src/com/modie/gitgud/Main.java
+++ src/com/modie/gitgud/Main.java
@@ -3,12 +3,19 @@ package com.modie.gitgud;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class Main extends Activity {
-
+	
+	Button btnPlay,btnLevel,btnTest,btnTodo,btnExit ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		btnPlay = (Button)findViewById(R.id.btnPlay);
+		btnLevel = (Button)findViewById(R.id.btnLevelCreator);
+		btnTest = (Button)findViewById(R.id.btnTests);
+		btnTodo = (Button)findViewById(R.id.btnTodo);
+		btnExit = (Button)findViewById(R.id.btnExit);
 		setContentView(R.layout.activity_main);
 	}
 
@@ -18,5 +25,7 @@ public class Main extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
 
 }
